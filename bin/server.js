342878f787720c0988c8f92261b46ed56ab4bf24
Jhen <developer@jhen.me>
2016-07-19 00:02:04 +0800

    Split options to another file

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,11 @@
+var assign = require('object-assign');
+var getOptions = require('./getOptions');
+
 module.exports = function(argv) {
   var SocketCluster = require('socketcluster').SocketCluster;
-
-  return new SocketCluster({
-    host: argv.hostname || process.env.npm_package_remotedev_hostname || null,
-    port: Number(argv.port || process.env.npm_package_remotedev_port) || 8000,
+  var options = assign(getOptions(argv), {
     workerController: __dirname + '/worker.js',
-    allowClientPublish: false,
-    protocol: argv.protocol || process.env.npm_package_remotedev_protocol || 'http',
-    protocolOptions: !(argv.protocol === 'https') ? null : {
-      key: argv.key || process.env.npm_package_remotedev_key || null,
-      cert: argv.cert || process.env.npm_package_remotedev_cert || null,
-      passphrase: argv.passphrase || process.env.npm_package_remotedev_passphrase || null
-    }
+    allowClientPublish: false
   });
+  return new SocketCluster(options);
 };
