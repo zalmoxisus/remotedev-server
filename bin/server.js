168206b9e0c4389ca5ac0795b1d22dccd6526d46
Jhen <developer@jhen.me>
2016-07-19 02:10:16 +0800

    Check port already used

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,7 @@
 var assign = require('object-assign');
+var repeat = require('repeat-string');
 var getOptions = require('./getOptions');
+var getPort = require('getport');
 
 module.exports = function(argv) {
   var SocketCluster = require('socketcluster').SocketCluster;
@@ -7,5 +9,19 @@ module.exports = function(argv) {
     workerController: __dirname + '/worker.js',
     allowClientPublish: false
   });
-  return new SocketCluster(options);
+  var port = options.port;
+  return new Promise(function(resolve) {
+    // Check port already used
+    getPort(port, function(err, p) {
+      if (err) return console.error(err);
+      if (port !== p) {
+        console.log('[RemoveDev] Server port ' + port + ' is already used.');
+        resolve({ portAlreadyUsed: true, on: function(status, cb) { cb(); } });
+      } else {
+        console.log('[RemoveDev] Start server...');
+        console.log(repeat('-', 80) + '\n');
+        resolve(new SocketCluster(options));
+      }
+    });
+  });
 };
