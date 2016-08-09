6eebee57b89e7584be15e385df2ac6c6891fcd3e
Rômulo Fernandes <romulo.fernandes@gmail.com>
2016-08-09 01:54:18 -0300

    Use logLevel config to suppress own console msgs.

--- bin/server.js
+++ bin/server.js
@@ -3,6 +3,12 @@ var repeat = require('repeat-string');
 var getOptions = require('./getOptions');
 var getPort = require('getport');
 
+
+var LOG_LEVEL_NONE = 0;
+var LOG_LEVEL_ERROR = 1;
+var LOG_LEVEL_WARN = 2;
+var LOG_LEVEL_INFO = 3;
+
 module.exports = function(argv) {
   var SocketCluster = require('socketcluster').SocketCluster;
   var options = assign(getOptions(argv), {
@@ -10,16 +16,26 @@ module.exports = function(argv) {
     allowClientPublish: false
   });
   var port = options.port;
+  var logLevel = options.logLevel === undefined ? LOG_LEVEL.INFO : options.logLevel;
   return new Promise(function(resolve) {
     // Check port already used
     getPort(port, function(err, p) {
-      if (err) return console.error(err);
+      if (err) {
+        if (logLevel >= LOG_LEVEL_ERROR) {
+          console.error(err);
+        }
+        return;
+      }
       if (port !== p) {
-        console.log('[RemoteDev] Server port ' + port + ' is already used.');
+        if (logLevel >= LOG_LEVEL_WARN) {
+          console.log('[RemoteDev] Server port ' + port + ' is already used.');
+        }
         resolve({ portAlreadyUsed: true, on: function(status, cb) { cb(); } });
       } else {
-        console.log('[RemoteDev] Start server...');
-        console.log(repeat('-', 80) + '\n');
+        if (logLevel >= LOG_LEVEL_INFO) {
+          console.log('[RemoteDev] Start server...');
+          console.log(repeat('-', 80) + '\n');
+        }
         resolve(new SocketCluster(options));
       }
     });
